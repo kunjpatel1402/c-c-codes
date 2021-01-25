b49b92b91bba69eb7703cb4f8635e78d36d844ec
kunjpatel1402 <75675988+kunjpatel1402@users.noreply.github.com>
2021-01-25 20:15:51 +0530

    Update path_finder_2.cpp

--- path_finder_2.cpp
+++ path_finder_2.cpp
@@ -1,11 +1,10 @@
 #include<iostream>
 #include<conio.h>
 #include<stdio.h>
-
 int a[23][33]={
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1,
     1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -24,84 +23,120 @@ int a[23][33]={
     1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,
     1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
-    1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
 };
+struct node{
+    int x,y;
+    node *next;
+}*start,*new_ptr,*ptr,*save;
+
+node *create_new_node(int x,int y){
+    new_ptr=new node;
+    new_ptr->x=x;
+    new_ptr->y=y;
+    new_ptr->next=NULL;
+    return new_ptr;
+}
+void join_node(node *ptr){
+    if (start==NULL){
+        start=ptr;
+    }
+    else{
+        ptr->next=start;
+        start=ptr;
+    }
+}
+void delete_node(){
+    if(start==NULL){
+        printf("\n empty list");
+    }
+    else{
+        ptr=start;
+        start=start->next;
+        delete ptr;
+    }
+}
 int x=1,y=1,solved=1;
-void print(int a[9][9]){
-    for (int row=0;row<9;row++){
-        for(int col=0;col<9;col++){
+void print(int a[23][33]){
+    for (int row=0;row<23;row++){
+        for(int col=0;col<33;col++){
             printf(" %d",a[row][col]);
         }
         printf("\n");
     }
 }
-
 int spread(){
     
     if (a[x][y+1]==0){
         a[x][y+1]=3;
         x=x;
         y=y+1;
+        ptr=create_new_node(x,y);
+        join_node(ptr);
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
     }
     else if (a[x][y+1]==2){
         printf("\n solved");
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
         return 0;
     }
     else if(a[x+1][y]==0){
         a[x+1][y]=3;
         x+=1;
         y=y;
+        ptr=create_new_node(x,y);
+        join_node(ptr);
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
     }
     else if(a[x+1][y]==2){
         printf("\n solved");
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
         return 0;
     }
     else if(a[x][y-1]==0){
         a[x][y-1]=3;
         x=x;
         y-=1;
+        ptr=create_new_node(x,y);
+        join_node(ptr);
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
     }
     else if(a[x][y-1]==2){
         printf("\n solved");
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
         return 0;
     }
     else if(a[x-1][y]==0){
         a[x-1][y]=3;
         x-=1;
         y=y;
+        ptr=create_new_node(x,y);
+        join_node(ptr);
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
     }
     else if(a[x-1][y]==2){
         printf("\n solved");
+        printf("\n x:%d",x);
+        printf("\n y:%d",y);
         return 0;
     }
     else{
-        if (a[x][y+1]==3){
-            a[x][y+1]=3;
-            a[x][y]=4;
-            x=x;
-            y=y+1;
-        }
-        else if (a[x+1][y]==3){
-            a[x+1][y]=3;
-            a[x][y]=4;
-            x=x+1;
-            y=y;
-        }
-        else if (a[x][y-1]==3){
-            a[x][y-1]=3;
-            a[x][y]=4;
-            x=x;
-            y=y-1;
-        }
-        else if (a[x-1][y]==3){
-            a[x-1][y]=3;
-            a[x][y]=4;
-            x-=1;
-            y=y;
-        }
+        a[x][y]=4;
+        delete_node();
+        x=start->x;
+        y=start->y;
     }
     printf("\n");
+    system("cls");
+    //print(a);
     printf("\n");
     return 1;
 }
@@ -123,14 +158,11 @@ void print_solved(){
 }
 int main(){
     system("cls");
-    print_solved();
-    int cont;
-    printf("\n cont:");scanf("%d",&cont);
     while(solved){
-        spread();
-        solved = spread();
-        system("cls");
         print_solved();
+        //int cont;
+        solved = spread();
+        //printf("\n cont:");scanf("%d",&cont);
     }
     return 0;
 }
